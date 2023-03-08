0b611748e64e1b6149ab8e3a5fb3cf6a0388d033
armcortex <armcortexfpga@gmail.com>
2023-03-09 03:52:01 +0800

    use hugo v0.53 version

--- scripts/run_server.sh
+++ scripts/run_server.sh
@@ -1,6 +1,6 @@
 docker run -d \
-    --name hugo-portfolio-tranquilpeak-20230306 \
+    --name hugo-portfolio-tranquilpeak-20230306-V0_53 \
     --mount type=bind,source=$(cd ../portfolio && pwd),target=/src \
     -p 1313:1313 \
-    klakegg/hugo:0.104.3-ext-ubuntu \
+    klakegg/hugo:0.53-ext-ubuntu \
     server --noHTTPCache
