7c1f9fda9d18d171622c8fe25bbf3a3a853eed93
armcortex <armcortexfpga@gmail.com>
2023-08-06 19:52:01 +0800

    update scripts

--- scripts/add_new_post.sh
+++ scripts/add_new_post.sh
@@ -4,4 +4,4 @@ docker run -it --rm \
     --mount type=bind,source=$(cd ../portfolio && pwd),target=/src \
     --entrypoint /bin/bash \
     klakegg/hugo:0.107.0-ext-ubuntu \
-    -c "hugo new post/${new_post}/${new_post}.md && cd content/post/${new_post} && mkdir images"
+    -c "hugo new post/${new_post}/index.md && cd content/post/${new_post} && mkdir images"
